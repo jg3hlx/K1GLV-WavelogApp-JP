52868dc54d485e6cb19e4e7f9c125af7ae17a426
Daniel Tang <dan@greenlava.net>
2026-01-11 20:00:28 -0500

    Fix compiler warning to rename Wavelog_Portable to all lower case

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:Wavelog_Portable/main.dart';
+import 'package:wavelog_portable/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
